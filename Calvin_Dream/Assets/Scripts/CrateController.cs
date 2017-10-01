1a2726b2c91469fb2683ca538b528ec85537fb90
Corey Laird <vindalesin@gmail.com>
2017-09-30 23:22:56 -0500

    Added box pickup/drop, made jumping more consistent.

---
+++ Calvin_Dream/Assets/Scripts/CrateController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour
+{
+    //bool prevFacingRight = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var player = GetComponentInParent<PlayerController>();
+        if (player != null)
+        {
+            var playerCollision = player.GetComponent<BoxCollider2D>();
+            var liftBody = GetComponentInChildren<Rigidbody2D>();
+            if (liftBody != null)
+            {
+                var liftCollisions = liftBody.GetComponentsInParent<BoxCollider2D>();
+                BoxCollider2D liftCollision = null;
+                foreach (var check in liftCollisions)
+                {
+                    if (!check.isTrigger)
+                    {
+                        liftCollision = check;
+                        break;
+                    }
+                }
+                
+                if (player.facingRight)
+                {
+                    this.transform.localPosition = new Vector2(playerCollision.offset.x + playerCollision.size.x + liftCollision.offset.x + liftCollision.size.x, 0f);
+                }
+                else
+                {
+                    this.transform.localPosition = new Vector2(playerCollision.offset.x - playerCollision.size.x - liftCollision.offset.x - liftCollision.size.x, 0f);
+                }
+            }
+        }
+	}
+}
--- Calvin_Dream/Assets/Scripts/PlayerController.cs
+++ Calvin_Dream/Assets/Scripts/PlayerController.cs
@@ -1,90 +1,170 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    public float speed;
-    public float jumpForce;
+public class PlayerController : MonoBehaviour
+{
+    public bool facingRight;
     public float groundTolerance = 0.5f;
-
-    private int floorLayer;
-
-    private float lastDistance;
+    public Transform heldObject;
+    public float jumpForce;
+    public float speed;
     private Rigidbody2D body;
     private new BoxCollider2D collider;
-    private float footPos;
+    private List<Collider2D> collidingTriggers;
+    private int floorLayer;
 
-    // Use this for initialization
-    void Start ()
+    private float footOffset;
+    private bool grounded;
+    private float lastDistance;
+    private bool liftPressed = false;
+
+    private void Start()
     {
         body = this.GetComponent<Rigidbody2D>();
         collider = this.GetComponent<BoxCollider2D>();
-        footPos = collider.size.y / 2.0f;
+        footOffset = collider.size.y / 2.0f;
         floorLayer = LayerMask.GetMask("Ground");
+        facingRight = true;
+        collidingTriggers = new List<Collider2D>();
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	}
 
-    void FixedUpdate()
+    private void Update()
     {
+        GetKeysDown();
+    }
+
+
+    private void FixedUpdate()
+    {
+        grounded = IsOnGround();
         HandleInput();
     }
 
-    bool IsOnGround()
+    private void GetKeysDown()
     {
-        var feet = body.position + collider.offset + Vector2.down * footPos;
-        var hit = Physics2D.Raycast(feet, Vector2.down, groundTolerance, floorLayer);
-        // Debug.DrawLine(feet, feet + Vector2.down * groundTolerance, Color.cyan);
-        if (hit)
+        liftPressed = liftPressed || Input.GetKeyDown(KeyCode.E);
+
+        if (liftPressed)
         {
-            // Check if the distance of the object hit is less than the last distance checked
-            if (hit.distance < lastDistance)
+            Debug.Log("Lift Pressed in Update");
+        }
+    }
+
+    private void HandleInput()
+    {
+        var currVelocity = body.velocity;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            currVelocity.x = speed;
+            body.velocity = currVelocity;
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            currVelocity.x = -speed;
+            body.velocity = currVelocity;
+            facingRight = false;
+        }
+        else
+        {
+            currVelocity.x = 0;
+            body.velocity = currVelocity;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && body.velocity.y <= 0)
+        {
+            if (grounded)
             {
-                // Update the last distance if the object below is less than the last known distance
-                lastDistance = hit.distance;
+                lastDistance = 100f;
+                body.AddForce(Vector2.up * jumpForce);
+            }
+        }
+
+        if (liftPressed)
+        {
+            Debug.Log("Lift Pressed in FixedUpdate");
+
+            var liftBody = heldObject.GetComponentInChildren<Rigidbody2D>();
+            if (heldObject.childCount > 0 && liftBody != null)
+            {
+                //Debug.Log("detach");
+
+                heldObject.DetachChildren();
+                liftBody.bodyType = RigidbodyType2D.Dynamic;
+                liftBody.velocity = body.velocity;
             }
             else
             {
-                // If the hit distance is not less than the lass distance, then you're probably not falling anymore.
-                lastDistance = 100f;
+                foreach (var trigger in collidingTriggers)
+                {
+                    if (trigger.CompareTag("Liftable"))
+                    {
+                        Lift(trigger);
+                        break;
+                    }
+                }
+            }
+
+            liftPressed = false;
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        var leftfoot = body.position + (Vector2.left * collider.size.x / 4.0f) + collider.offset + Vector2.down * footOffset;
+        var rightfoot = body.position + (Vector2.right * collider.size.x / 4.0f) + collider.offset + Vector2.down * footOffset;
+
+        var leftHit = Physics2D.Raycast(leftfoot, Vector2.down, groundTolerance, floorLayer);
+        var rightHit = Physics2D.Raycast(rightfoot, Vector2.down, groundTolerance, floorLayer);
+
+        Debug.DrawLine(leftfoot, leftfoot + Vector2.down * groundTolerance, Color.blue);
+        Debug.DrawLine(rightfoot, rightfoot + Vector2.down * groundTolerance, Color.blue);
+
+        if (leftHit || rightHit)
+        {
+            // Check if the distance of the object hit is less than the last distance checked
+            if (leftHit.distance < lastDistance)
+            {
+                // Update the last distance if the object below is less than the last known distance
+                lastDistance = leftHit.distance;
+            }
+            else if (rightHit.distance < lastDistance)
+            {
+                lastDistance = rightHit.distance;
+            }
+            else
+            {
+                // If the hit distance is not less than the last distance, then you're probably not
+                // falling anymore.
+                //Debug.Log("On ground");
                 return true;
             }
         }
 
+        //Debug.Log("Not on ground");
         return false;
     }
 
-    void HandleInput()
+    private void Lift(Collider2D trigger)
     {
-        if (IsOnGround())
-        {
-            var currVelocity = body.velocity;
+        var liftTransform = trigger.GetComponentInParent<Transform>();
+        var liftBody = trigger.GetComponentInParent<Rigidbody2D>();
 
-            if (Input.GetAxis("Horizontal") > 0)
-            {
-                currVelocity.x = speed;
-                body.velocity = currVelocity;
-            }
-            else if (Input.GetAxis("Horizontal") < 0)
-            {
-                currVelocity.x = -speed;
-                body.velocity = currVelocity;
-            }
-            else
-            {
-                currVelocity.x = 0;
-                body.velocity = currVelocity;
-            }
+        //Debug.Log("attach");
 
-            if (Input.GetAxis("Jump") > 0)
-            {
-                body.AddForce(Vector2.up * jumpForce);
-            }
-        }
+        liftBody.bodyType = RigidbodyType2D.Kinematic;
+        liftTransform.SetParent(heldObject);
+        liftTransform.localPosition = Vector3.zero;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collidingTriggers.Add(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collidingTriggers.Remove(collision);
+    }
 }
