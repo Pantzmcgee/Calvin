abf4b6472bbc568f227380fa4b15f5cb2a4485a3
Pantzmcgee <pantzmcgee@gmail.com>
2017-09-30 23:45:39 -0500

    audio trigger

---
+++ Calvin_Dream/Assets/Scripts/Audio_Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Trigger : MonoBehaviour {
+
+    public AudioSource source;
+    private bool check = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (check)
+        {
+            Debug.Log("Bacon");
+            source.Play();
+            check = false;
+        }
+    }
+}
