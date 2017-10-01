f9b163744cd9067f294ff3c3c9ac810d782a54e6
Pantzmcgee <pantzmcgee@gmail.com>
2017-10-01 17:11:19 -0500

    Added Menus and Navigation
    Added Menus, Credits, Thanks for playing and navigation for it.
    Also fixed velocity bug with no friction floors on crates.

---
+++ Calvin_Dream/Assets/Scripts/Menu_ToggleControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu_ToggleControl : MonoBehaviour {
+
+    public GameObject Calvin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //changes position of Calvin
+        if (Calvin.transform.position.y > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Calvin.transform.position = new Vector3((float)-1.3, (float)-.6,(float)-1);
+            }
+        }
+        else if(Calvin.transform.position.y < 0 && Calvin.transform.position.y > -1)
+        {
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Calvin.transform.position = new Vector3((float)-1.3, (float)-1.3, (float)-1);
+            }
+            else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Calvin.transform.position = new Vector3((float)-1.3, (float).1, (float)-1);
+            }
+        }
+        else if (Calvin.transform.position.y < -1)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Calvin.transform.position = new Vector3((float)-1.3, (float)-.6, (float)-1);
+            }
+        }
+
+        //Pressing return key will load correct scene
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if (Calvin.transform.position.y > 0)
+            {
+                SceneManager.LoadScene("LevelOne");
+            }
+            else if (Calvin.transform.position.y < 0 && Calvin.transform.position.y > -1)
+            {
+                SceneManager.LoadScene("Credits");
+            }
+            else if (Calvin.transform.position.y < -1)
+            {
+                Application.Quit();
+            }
+        }
+
+    }
+}
---
+++ Calvin_Dream/Assets/Scripts/Velocity_Zero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity_Zero : MonoBehaviour {
+    public Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (rb2d.velocity.x != 0)
+        {
+            if (rb2d.velocity.x < 1 && rb2d.velocity.x > -1)
+            {
+                rb2d.velocity = new Vector2(0, 0);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2((rb2d.velocity.x) * .5f, rb2d.velocity.y);
+            }
+        }
+		
+	}
+}
